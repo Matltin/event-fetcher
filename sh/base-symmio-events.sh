f05c7fa702f1408a9b7201d4b03deae918c78c3f
Matltin <ma1383ahmadi@gmail.com>
2025-07-24 15:37:54 +0330

    add sh files

---
+++ sh/base-symmio-events.sh
@@ -0,0 +1,3 @@
+export CONTRACT_ADDRESS="0x91Cf2D8Ed503EC52768999aA6D8DBeA6e52dbe43"
+export RPC_URL="https://0xrpc.io/base"
+go run service.go
---
+++ sh/polygon-symmio.sh
@@ -0,0 +1,3 @@
+export CONTRACT_ADDRESS="0x976c87Cd3eB2DE462Db249cCA711E4C89154537b"
+export RPC_URL="https://polygon-rpc.com"
+go run ./cmd/eventsdb/main.go
