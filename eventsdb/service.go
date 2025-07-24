1884007741a6eec7a3968231b43ab1d00a695712
Matltin <ma1383ahmadi@gmail.com>
2025-07-24 08:06:08 +0330

    init database

--- eventsdb/service.go
+++ eventsdb/service.go
@@ -1,6 +1,7 @@
 package eventsdb
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -22,7 +23,24 @@ func NewIndexerService(config Config) *IndexerService {
 }
 
 func (s *IndexerService) Start() error {
+	// Print confuguration
 	s.printConfiguration()
+
+	// Initialize database
+	if err := s.initializeDatabase(); err != nil {
+		return fmt.Errorf("failed to initialize database: %w", err)
+	}
+
+	// Load event on database
+	if err := s.loadEventSignaturesOnDB(); err != nil {
+		return fmt.Errorf("failed to store event on db : %w", err)
+	}
+
+	// Load event signatures from database
+	if err := s.loadEventSignatures(); err != nil {
+		log.Printf("Warning: Failed to load event signatures: %v\n", err)
+		log.Println("Continuing without event signature decoding...")
+	}
 
 	return nil
 }
@@ -37,4 +55,14 @@ func (s *IndexerService) printConfiguration() {
 	log.Printf("  Retry Delay: %v\n", s.config.RetryDelay)
 	log.Printf("  GORM Logs: %t\n", s.config.EnableGormLogs)
 	log.Printf("  Postgres: %s:%s@%s:%s/%s\n", s.config.PgUser, "******", s.config.PgHost, s.config.PgPort, s.config.PgDbName)
+}
+
+func (s *IndexerService) initializeDatabase() error {
+	db, err := initDB(s.config)
+	if err != nil {
+		return err
+	}
+	s.db = db
+	fmt.Println("Successfully connected to PostgreSQL database")
+	return nil
 }
